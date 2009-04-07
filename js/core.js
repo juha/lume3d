e7564321c2411479d664f26e9adce1197c65e8c0
Juha <juha.toronen@gmail.com>
2009-04-07 13:22:40 +0300

    First pages commit

--- js/core.js
+++ js/core.js
@@ -2,8 +2,15 @@ jQuery.fn.navigation = function () {
     var h3 = this.find("h3");
     var ul = this.find("ul");
     h3.toggle(
-        function () {ul.show(); },
-        function () {ul.hide(); }  
+        function () {
+            ul.show();
+            $("body").click(function () {
+                ul.hide();
+            })
+        },
+        function () {
+            ul.hide();
+        }
     );
 };
 
@@ -11,10 +18,13 @@ jQuery.fn.swf = function (xml) {
     if (!this.length) return;
     var href = this.find("a").attr("href");
     if (href) {
-        var so = new SWFObject(href, "content-flash", "800", "400", "9", "#000000"); 	
+        var so = new SWFObject(href, "flash-el", "100%", "600", "9", "#000000"); 	
         so.addParam("allowFullScreen","true");
     	so.addParam("allowScriptAccess","sameDomain");
     	so.write(this.attr("id"));
+    	
+    	var f = document.getElementById("flash-el");
+    	alert(f.moduleLoader)
     }
 };
 
