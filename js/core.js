ee7b33660e222a68bf4064188cbdc3bb2343489a
Juha <juha.toronen@gmail.com>
2009-04-07 15:11:52 +0300

    Room details

--- js/core.js
+++ js/core.js
@@ -18,14 +18,33 @@ jQuery.fn.swf = function (xml) {
     if (!this.length) return;
     var href = this.find("a").attr("href");
     if (href) {
-        var so = new SWFObject(href, "flash-el", "100%", "600", "9", "#000000"); 	
+        var so = new SWFObject(href, "flash-el", "100%", "600", "9", "#ffffff"); 	
         so.addParam("allowFullScreen","true");
     	so.addParam("allowScriptAccess","sameDomain");
     	so.write(this.attr("id"));
     }
 };
 
+jQuery.fn.toggleTarget = function () {
+    var id = this.attr("href");
+    var el = $(id);
+    if (el.length) {
+        this.toggle(function () {
+            this._innerHTML = this.innerHTML
+            this.innerHTML = this.innerHTML.replace("Piilota", "Näytä");
+            el.hide();
+        }, function () {
+            this.innerHTML = this._innerHTML;
+            el.show();
+        });
+    }
+};
+
 $(document).ready(function () {
     $("#top-nav").navigation();
     $("#content-flash").swf();
+    $("#sidebar a.toggle").toggleTarget();
+    $("#content-body").click(function () {
+        $(this).hide();
+    });
 });
